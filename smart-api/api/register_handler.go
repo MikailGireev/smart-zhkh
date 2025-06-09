4bb3526ea36c35f4f5ed029f746b2be71ae0f11e
mac <mac@MacBook-Pro-mac.local>
2025-06-09 09:59:50 -0700

    feat: add httpx/json_error -> NewJSONError function

--- smart-api/api/register_handler.go
+++ smart-api/api/register_handler.go
@@ -4,28 +4,30 @@ import (
 	"encoding/json"
 	"net/http"
 	"smart-api/internal/auth"
+	"smart-api/internal/httpx"
 )
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
+		httpx.NewJSONError(w, http.StatusMethodNotAllowed, "Method not allowed", "Method not allowed")
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return 
 	}
 
 	var req auth.LoginRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid request", http.StatusBadRequest)
+		httpx.NewJSONError(w, http.StatusBadRequest, "Invalid request", "Invalid request")
 		return
 	}
 
 	if req.Username == "" || req.Password == "" {
-		http.Error(w, "Username and password are required", http.StatusBadRequest)
+		httpx.NewJSONError(w, http.StatusBadRequest, "Invalid request", "Username and password are required")
 		return
 	}
 
 	err := auth.RegisterUser(req.Username, req.Password)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		httpx.NewJSONError(w, http.StatusInternalServerError, "User regsistered successfully", err.Error())
 		return
 	}
 
